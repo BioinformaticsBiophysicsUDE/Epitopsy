0c66130ec292f6b04bd47cd337815631e6d649f5
Jean-Noel Grad <jean-noel.grad@uni-due.de>
2017-01-31 18:55:32 +0100

    Add function with timer

--- epitopsy/tools/UtilityClasses.py
+++ epitopsy/tools/UtilityClasses.py
@@ -1,13 +1,15 @@
-'''
-Created on Jun 29,  2011
-
-@author: Christoph Wilms
-'''
+__author__     = "Christoph Wilms, Jean-Noel Grad"
+__copyright__  = "Copyright 2011, Epitopsy"
+__date__       = "Jun 29,  2011"
+__credits__    = ["Christoph Wilms", "Jean-Noel Grad"]
+__license__    = "LGPL v3"
+__doc__        = """Various tools."""
 
 import time
 import sys
+import datetime
+import numpy as np
 
-from numpy import ceil, mod, array
 
 class Progress_bar:
     '''
@@ -28,20 +30,170 @@ class Progress_bar:
         self.start_time = time.time()
         self.progress_bar = ''
         self.refresh = 15
-        print('0%                                             100%\n#                                                 #')
+        print('0%                                             100%\n'
+              '#                                                 #')
     def add(self):
         self.finished = self.finished + 1
         if self.finished > self.number_of_item:
             return
         elif self.finished == self.number_of_item:
+            time_spent = round((time.time() - self.start_time) / 60., 2)
             print('#*****************    DONE    ********************#')
-            print('Calculations finished after {0} min.'.format(round((time.time() - self.start_time) / 60., 2)))
+            print('Execution time: {0} min.'.format(time_spent))
             return
         elif int(self.finished / self.number_of_item * 100. / 2.) > self.counter * 50. / self.refresh:
             self.counter = self.counter + 1
             self.progress_bar = '*' * int(self.finished / self.number_of_item * 100. / 2.)
             self.progress_bar = self.progress_bar + ' ' * int(49. - len(self.progress_bar)) + '#'
             print('{0}{1}'.format('#', self.progress_bar))
+
+class Progress_bar_countdown(object):
+    """
+    Display a progress bar, 70 characters wide, with countdown if requested.
+    The countdown uses a linear combination of the 4 last stored timings to
+    compute the remaining time.
+    
+    :param iterations: total number of iterations
+    :type  iterations: int
+    :param refresh: number of iteration to complete before refreshing the
+        progress bar (optional), default is to update every 10 %, namely for
+        **iterations** = 150, the progress bar is updated every 15 increments;
+        can be given as a percentage (0.10) or as a number of increments (15)
+    :type refresh: int or float
+    :param countdown: display a countdown (optional), default is ``False``
+    :type  countdown: bool
+    
+    .. attribute:: iterations
+    
+        total number of iterations
+    
+    .. attribute:: counter
+    
+        current state of the iteration
+    
+    .. attribute:: finished
+    
+        ``True`` if iteration has finished, prevents the progress bar from
+        overflowing in the case where **iterations** was underestimated
+    
+    .. attribute:: checkpoints
+    
+        all checkpoints to pass to get an update of the progress bar
+    
+    .. attribute:: timer
+    
+        timing of each iteration
+    
+    
+    The progress bar takes 70 characters with countdown,  50 otherwise.
+    
+    Example::
+    
+        >>> from epitopsy.progress_bar import Progress_bar
+        >>> progress_bar = Progress_bar_countdown(len(array), refresh = 1, countdown = True)
+        >>> for i in range(len(array)):
+        ...     function(array[i])
+        ...     progress_bar.iterate()
+        0%                                            100%   time left
+        #+++++++++++++++++++++++++++++++++++++++         #   0:01:36        
+
+    """
+    
+    def __init__(self, iterations, refresh = None, countdown = False):
+        self.iterations = iterations
+        self.counter = 0
+        self.finished = False
+        self.start_time = time.time()
+        self.timer = []
+        
+        # rangecheck: refresh
+        if not refresh or refresh > iterations/4 or refresh < 0: # if no value or illegal value
+            refresh = max(1, int(iterations/10.))                # set to 10% of **iterations** (minimum value of 1)
+        elif type(refresh) == float:
+            if 0.0 < refresh < 1.0:                              # if percentage, multiply by **iterations**
+                refresh = int(refresh * iterations)
+            else:                                                # if not, consider as an int
+                refresh = int(refresh)
+        elif type(refresh) == int:                               # if already int, use it as is
+            pass
+        
+        # use refresh to create checkpoints
+        self.checkpoints = np.array(range(refresh, iterations, refresh) + [iterations])
+        
+        # print first line
+        if countdown:
+            self.timer.append([0,0])
+            print("0%                                            100%   time left")
+        else:
+            print("0%                                            100%")
+    
+    
+    def increment(self, amount = 1):
+        """
+        Increment the counter of **amount** unit(s).
+        
+        :param amount: value of the increment (optional), default is unity
+        :type  amount: int
+        
+        :returns: 0 if no error, 1 if :attr:`finished` was ``True``
+        """
+        
+        if self.finished:
+            return 1
+        
+        self.counter += amount
+        
+        # managing countdown
+        if self.timer:
+            self.timer.append([self.counter, time.time() - self.start_time])
+        remains = ''
+        
+        if np.any(self.counter >= self.checkpoints):
+            if self.counter >= self.iterations:
+                self.terminate()
+            else:
+                if len(self.timer) >= 4:
+                    # the countdown is a linear combination of the last 4
+                    # durations with weights [1, 0.5, 0.33, 0.25]
+                    weights = 1 / np.arange(1, 5, dtype = float)[::-1]
+                    val = np.array(self.timer[-5:], dtype = float)
+                    spent = (val[1:,1] - val[:-1,1]) / (val[1:,0] - val[:-1,0])
+                    spent = np.sum(spent * weights) / np.sum(weights)
+                    remains = int(spent * (self.iterations - self.counter))
+                    remains = "   {0:<15}".format(datetime.timedelta(seconds = remains))
+                bar = "\r#{0:<48}#".format(int(48 * self.counter / float(self.iterations)) * '+')
+                sys.stdout.write(bar + remains)
+                sys.stdout.flush()
+                self.checkpoints = np.array([x for x in self.checkpoints if x > self.counter])
+        
+        return 0
+        
+    def add(self):
+        """
+        Alias of :meth:`Progress_bar.increment` for retro-compatibility.
+        Add 1 to the counter.
+        """
+        
+        self.increment(amount = 1)
+    
+    def terminate(self):
+        """
+        Terminate the timer and print "DONE" to screen.
+        
+        :returns: 0 if no error, 1 if :attr:`finished` was already ``True``
+        """
+        
+        if self.finished:
+            return 1
+        
+        self.finished = True
+        
+        if self.timer:
+            print "\r#{0:+^48}#   {1:<15}".format("   DONE   ", datetime.timedelta(seconds = 0))
+        else:
+            print "\r#{0:+^48}#".format("   DONE   ")
+        
+        return 0
 
 class Fix_xxmer:
     '''
@@ -190,9 +342,10 @@ class ESP_Profile_Manager:
                 for item in line_content:
                     phi_list.append(float(item))
         if phi_values == True:
-            return [array(xlist), array(ylist), array(zlist)], array(phi_list)
+            return ([np.array(xlist), np.array(ylist), np.array(zlist)],
+                                                      np.array(phi_list))
         else:
-            return [array(xlist), array(ylist), array(zlist)]
+            return [np.array(xlist), np.array(ylist), np.array(zlist)]
 
     def write_hull_coordinates(self, hull_coordinates, filename = 'hull_coordinates.txt'):
         """
